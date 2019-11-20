b54c9f30b74fbb46f091297714766892371cc40f
Ariel <arielladret@gmail.com>
2019-11-20 09:40:47 -0800

    added fonts

--- functions.php
+++ functions.php
@@ -58,13 +58,18 @@ add_filter( 'stylesheet_uri', 'qod_minified_css', 10, 2 );
 function qod_scripts() {
 	wp_enqueue_style( 'qod-style', get_stylesheet_uri() );
 
-wp_enqueue_script( 'qod-script', get_template_directory_uri() . '/build/js/qod.min.js', array('jquery'), '', true );
+	wp_enqueue_style( 'qod-fonts', 'https://fonts.googleapis.com/css?family=Exo:300,300i,500,500i&display=swap' );
+
+
+wp_enqueue_script( 'qod-script', get_template_directory_uri() . '/build/js/qod.min.js', array('jquery'),false, true );
 	wp_enqueue_script( 'qod-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'qod-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
 	//here is where you localized script code will go, reference this script qod
 	//look at the WP REST API slides or the wp-2017 rest theme in functions.php for more info
 }
 add_action( 'wp_enqueue_scripts', 'qod_scripts' );
+
+
 
 /**
  * Custom functions that act independently of the theme templates.
--- header.php
+++ header.php
@@ -12,19 +12,27 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet"> 
 
 	<?php wp_head(); ?>
 	</head>
 
 	<body <?php body_class(); ?>>
+	<img class="site-logo" src="<?php echo get_template_directory_uri(); ?>../../project-05/assets/qod-logo.svg" />
+
+
 		<div id="page" class="hfeed site">
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
+
+
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
+
+			
 
 			
 				</nav><!-- #site-navigation -->
